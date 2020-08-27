50fe2e9df874de19d829bf138d78f65814efa7e6
Khoa Nguyen <khoadnguyen.se@gmail.com>
2020-08-27 22:39:56 +0800

    add binary tree depth first traversals

--- data_structures/trees/binary_tree.h
+++ data_structures/trees/binary_tree.h
@@ -31,10 +31,20 @@ class BinaryTree {
   ~BinaryTree();
   int Size() const;
   bool IsEmpty() const;
+  void Insert();
+  T Remove();
 
  private:
   Node *root_;
   int size_;
+
+ private:
+  // Depth First Traversals
+  void PreOrder(Node *node) const;
+  void InOrder(Node *node) const;
+  void PostOrder(Node *node) const;
+  // Breath First Traversal
+  void LevelOrder(Node *node) const;
 };
 
 template <class T>
@@ -53,6 +63,45 @@ int BinaryTree<T>::Size() const {
 template <class T>
 bool BinaryTree<T>::IsEmpty() const {
   return (size_ == 0);
+}
+
+template <class T>
+void BinaryTree<T>::Insert() {
+
+}
+
+template <class T>
+T BinaryTree<T>::Remove() {
+  return nullptr;
+}
+
+template <class T>
+void BinaryTree<T>::PreOrder(BinaryTree::Node *node) const {
+  if (node == NULL) return;
+  std::cout << node->data_ << " ";
+  PreOrder(node->left_);
+  PreOrder(node->right_);
+}
+
+template <class T>
+void BinaryTree<T>::InOrder(BinaryTree::Node *node) const {
+  if (node == NULL) return;
+  InOrder(node->left_);
+  std::cout << node->data_ << " ";
+  InOrder(node->right_);
+}
+
+template <class T>
+void BinaryTree<T>::PostOrder(BinaryTree::Node *node) const {
+  if (node == NULL) return;
+  PostOrder(node->left_);
+  PostOrder(node->right_);
+  std::cout << node->data << " ";
+}
+
+template <class T>
+void BinaryTree<T>::LevelOrder(BinaryTree::Node *node) const {
+
 }
 
 }  // namespace trees
