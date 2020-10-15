439568c0b6c1f0f1aa5f0b55765492c2b36a47c9
Khoa Nguyen <khoadnguyen.se@gmail.com>
2020-10-15 21:14:49 +0800

    update binary tree

--- data_structures/trees/binary_tree.cc
+++ data_structures/trees/binary_tree.cc
@@ -1,5 +1,34 @@
 #include "binary_tree.h"
 
 int main() {
-    return 0;
+  std::cout << "TEST BINARY TREE\n" << std::endl;
+  data_structures::trees::BinaryTree<int> binary_tree;
+  std::cout << "Empty: " << binary_tree.IsEmpty() << std::endl;
+  std::cout << "Size: " << binary_tree.Size() << std::endl;
+  std::cout << "Insert 1" << std::endl;
+  binary_tree.Insert(1);
+  std::cout << "Insert 2" << std::endl;
+  binary_tree.Insert(2);
+  std::cout << "Insert 3" << std::endl;
+  binary_tree.Insert(3);
+  std::cout << "Insert 4" << std::endl;
+  binary_tree.Insert(4);
+  std::cout << "Insert 5" << std::endl;
+  binary_tree.Insert(5);
+  std::cout << "Insert 6" << std::endl;
+  binary_tree.Insert(6);
+  std::cout << "Insert 7" << std::endl;
+  binary_tree.Insert(7);
+  std::cout << binary_tree << std::endl;
+  std::cout << "Empty: " << binary_tree.IsEmpty() << std::endl;
+  std::cout << "Size: " << binary_tree.Size() << std::endl;
+  std::cout << "Search 6: " << binary_tree.Search(6)->data_ << std::endl;
+  std::cout << "Search 2: " << binary_tree.Search(2)->data_ << std::endl;
+  std::cout << "Remove 4: " << binary_tree.Remove(4) << std::endl;
+  std::cout << "Size: " << binary_tree.Size() << std::endl;
+  std::cout << "Remove 1: " << binary_tree.Remove(1) << std::endl;
+  std::cout << "Size: " << binary_tree.Size() << std::endl;
+  std::cout << binary_tree << std::endl;
+
+  return 0;
 }
--- data_structures/trees/binary_tree.h
+++ data_structures/trees/binary_tree.h
@@ -2,7 +2,9 @@
 #define ALGORITHMS_DATA_STRUCTURES_TREES_BINARY_TREE_H_
 
 #include <iostream>
+
 #include "data_structures/queues/queue.h"
+#include "data_structures/stacks/stack.h"
 
 namespace data_structures {
 namespace trees {
@@ -11,14 +13,12 @@ template <class T>
 class BinaryTree {
  public:
   class Node {
-   private:
+   public:
     T data_;
     Node *parent_;
     Node *left_;
     Node *right_;
     friend class BinaryTree;
-
-   public:
     explicit Node(const T &data) : data_(data), parent_(NULL), left_(NULL), right_(NULL) {}
   };
 
@@ -27,47 +27,63 @@ class BinaryTree {
   ~BinaryTree();
   int Size() const;
   bool IsEmpty() const;
-  const Node &Search(const T &e) const;
-  void Insert(const T &e);
-  T Remove(const T &e);
+  const Node *Search(const T &data) const;
+  void Insert(const T &data);
+  T Remove(const T &data);
 
   friend std::ostream &operator<<(std::ostream &out, const BinaryTree &tree) {
     if (tree.IsEmpty()) {
-      out << "Empty tree" << std::endl;
+      out << "Empty tree";
     } else {
       out << "Preorder traversal: ";
-      tree.PreOrder(tree.root_);
+      tree.PreOrder(out, tree.root_, true);
+      out << std::endl;
+
+      out << "Non-recursive preorder traversal: ";
+      tree.PreOrder(out, tree.root_, false);
       out << std::endl;
 
       out << "Inorder traversal: ";
-      tree.InOrder(tree.root_);
+      tree.InOrder(out, tree.root_, true);
+      out << std::endl;
+
+      out << "Non-recursive inorder traversal: ";
+      tree.InOrder(out, tree.root_, false);
       out << std::endl;
 
       out << "Postorder traversal: ";
-      tree.PostOrder(tree.root_);
+      tree.PostOrder(out, tree.root_, true);
+      out << std::endl;
+
+      out << "Non-recursive postorder traversal: ";
+      tree.PostOrder(out, tree.root_, false);
       out << std::endl;
 
       out << "Levelorder traversal: ";
-      tree.LevelOrder(tree.root_);
+      tree.LevelOrder(out, true);
       out << std::endl;
+
+      out << "Non-recursive levelorder traversal: ";
+      tree.LevelOrder(out, false);
     }
     return out;
   }
 
+ protected:
+  const Node *Search(const Node *node, const T &data) const;
+  // Depth First Traversals
+  void PreOrder(std::ostream &out, Node *node, bool recursive) const;
+  void InOrder(std::ostream &out, Node *node, bool recursive) const;
+  void PostOrder(std::ostream &out, Node *node, bool recursive) const;
+  // Breadth First Traversal
+  void LevelOrder(std::ostream &out, bool recursive) const;
+  void PrintLevel(std::ostream &out, Node *node, int level) const;
+  int Height(Node *node) const;
+  void Transplant(Node *node, Node *child);
+
  private:
   Node *root_;
   int size_;
-
- private:
-  const Node &Search(const Node &node, const T &e) const;
-  // Depth First Traversals
-  std::ostream &PreOrder(std::ostream &out, Node *node) const;
-  std::ostream &InOrder(std::ostream &out, Node *node) const;
-  std::ostream &PostOrder(std::ostream &out, Node *node) const;
-  // Breath First Traversal
-  std::ostream &LevelOrder(std::ostream &out) const;
-  std::ostream &PrintLevel(std::ostream &out, Node *node, int level) const;
-  int Height(Node *node) const;
 };
 
 template <class T>
@@ -85,30 +101,29 @@ int BinaryTree<T>::Size() const {
 
 template <class T>
 bool BinaryTree<T>::IsEmpty() const {
-  return (size_ == 0);
+  return (root_ == NULL);
 }
 
 template <class T>
-const class BinaryTree<T>::Node &BinaryTree<T>::Search(const T &e) const {
-  return Search(root_, e);
+const typename BinaryTree<T>::Node *BinaryTree<T>::Search(const T &data) const {
+  return Search(root_, data);
 }
 
 template <class T>
-const class BinaryTree<T>::Node &BinaryTree<T>::Search(const BinaryTree::Node &node, const T &e) const {
-  if (node == NULL) return NULL;
-  if (node->data_ == e) return node;
-  Node &left_search = Search(node.left_, e);
+const typename BinaryTree<T>::Node *BinaryTree<T>::Search(const BinaryTree::Node *node, const T &data) const {
+  if (node == NULL || node->data_ == data) return node;
+  const Node *left_search = Search(node->left_, data);
   if (left_search != NULL) return left_search;
-  return Search(node.right_, e);
+  return Search(node->right_, data);
 }
 
 template <class T>
-void BinaryTree<T>::Insert(const T &e) {
-  Node *node = new Node(e);
-  if (IsEmpty()) {
+void BinaryTree<T>::Insert(const T &data) {
+  Node *node = new Node(data);
+  if (IsEmpty())
     root_ = node;
-  } else {
-    data_structures::queues::Queue<Node> queue(size_ + 1);
+  else {
+    data_structures::queues::Queue<Node *> queue(size_ + 1);
     queue.Enqueue(root_);
     while (!queue.IsEmpty()) {
       Node *temp = queue.Dequeue();
@@ -117,21 +132,52 @@ void BinaryTree<T>::Insert(const T &e) {
         temp->left_ = node;
         node->parent_ = temp;
         break;
-      } else queue.Enqueue(temp->left_);
+      } else
+        queue.Enqueue(temp->left_);
 
       if (temp->right_ == NULL) {
         temp->right_ = node;
         node->parent_ = temp;
         break;
-      } else queue.Enqueue(temp->right_);
+      } else
+        queue.Enqueue(temp->right_);
     }
   }
   size_++;
 }
 
 template <class T>
-T BinaryTree<T>::Remove(const T &e) {
-  return e;
+T BinaryTree<T>::Remove(const T &data) {
+  if (IsEmpty()) throw std::runtime_error("Tree is empty, can not remove");
+  Node *removed = NULL;
+  Node *node = NULL;
+  data_structures::queues::Queue<Node *> queue(size_ + 1);
+  queue.Enqueue(root_);
+  while (!queue.IsEmpty()) {
+    node = queue.Dequeue();
+    if (node->data_ == data) removed = node;
+    if (node->left_ != NULL) queue.Enqueue(node->left_);
+    if (node->right_ != NULL) queue.Enqueue(node->right_);
+  }
+  if (removed == NULL) throw std::runtime_error("Node is not exist in tree");
+  T saved_data = node->data_;
+  T removed_data = removed->data_;
+  Transplant(node, NULL);
+  removed->data_ = saved_data;
+  size_--;
+  return removed_data;
+}
+
+template <class T>
+void BinaryTree<T>::Transplant(Node *node, Node *child) {
+  if (node->parent_ == NULL)
+    root_ = child;
+  else if (node == node->parent_->left_)
+    node->parent_->left_ = child;
+  else
+    node->parent_->right_ = child;
+  if (child != NULL) child->parent_ = node->parent_;
+  node->parent_ = NULL;
 }
 
 template <class T>
@@ -139,52 +185,112 @@ int BinaryTree<T>::Height(BinaryTree::Node *node) const {
   if (node == NULL)
     return 0;
   else
-    return 1 + std::max(Height(node->left), Height(node->right));
+    return 1 + std::max(Height(node->left_), Height(node->right_));
 }
 
 template <class T>
-std::ostream &BinaryTree<T>::PreOrder(std::ostream &out, BinaryTree::Node *node) const {
-  if (node == NULL) return out;
-  out << node->data_ << " ";
-  PreOrder(node->left_);
-  PreOrder(node->right_);
-  return out;
-}
-
-template <class T>
-std::ostream &BinaryTree<T>::InOrder(std::ostream &out, BinaryTree::Node *node) const {
-  if (node == NULL) return out;
-  InOrder(node->left_);
-  out << node->data_ << " ";
-  InOrder(node->right_);
-  return out;
-}
-
-template <class T>
-std::ostream &BinaryTree<T>::PostOrder(std::ostream &out, BinaryTree::Node *node) const {
-  if (node == NULL) return out;
-  PostOrder(node->left_);
-  PostOrder(node->right_);
-  out << node->data << " ";
-  return out;
-}
-
-template <class T>
-std::ostream &BinaryTree<T>::LevelOrder(std::ostream &out) const {
-  for (int i = 1; i <= Height(root_); i++) out << PrintLevel(root_, i);
-  return out;
-}
-
-template <class T>
-std::ostream &BinaryTree<T>::PrintLevel(std::ostream &out, BinaryTree::Node *node, int level) const {
-  if (node == NULL) return out;
-  if (level == 1) {
-    out << node->data_ << " ";
-    return out;
+void BinaryTree<T>::PreOrder(std::ostream &out, BinaryTree::Node *node, bool recursive) const {
+  if (recursive) {
+    if (node == NULL) return;
+    out << "[" << node->data_ << "]->";
+    PreOrder(out, node->left_, recursive);
+    PreOrder(out, node->right_, recursive);
+  } else {
+    Node *temp = root_;
+    data_structures::stacks::Stack<Node *> stack(size_);
+    while (true) {
+      if (temp != NULL) {
+        stack.Push(temp);
+        out << "[" << temp->data_ << "]->";
+        temp = temp->left_;
+      } else if (!stack.IsEmpty()) {
+        temp = stack.Pop();
+        temp = temp->right_;
+      } else
+        break;
+    }
   }
-  PrintLevel(node->left_, level - 1);
-  PrintLevel(node->right_, level - 1);
-  return out;
+}
+
+template <class T>
+void BinaryTree<T>::InOrder(std::ostream &out, BinaryTree::Node *node, bool recursive) const {
+  if (recursive) {
+    if (node == NULL) return;
+    InOrder(out, node->left_, recursive);
+    out << "[" << node->data_ << "]->";
+    InOrder(out, node->right_, recursive);
+  } else {
+    Node *temp = root_;
+    data_structures::stacks::Stack<Node *> stack(size_);
+    while (true) {
+      if (temp != NULL) {
+        stack.Push(temp);
+        temp = temp->left_;
+      } else if (!stack.IsEmpty()) {
+        temp = stack.Pop();
+        out << "[" << temp->data_ << "]->";
+        temp = temp->right_;
+      } else
+        break;
+    }
+  }
+}
+
+template <class T>
+void BinaryTree<T>::PostOrder(std::ostream &out, BinaryTree::Node *node, bool recursive) const {
+  if (recursive) {
+    if (node == NULL) return;
+    PostOrder(out, node->left_, recursive);
+    PostOrder(out, node->right_, recursive);
+    out << "[" << node->data_ << "]->";
+  } else {
+    Node *temp = root_;
+    data_structures::stacks::Stack<Node *> stack(size_);
+    do {
+      while (temp != NULL) {
+        if (temp->right_ != NULL) stack.Push(temp->right_);
+        stack.Push(temp);
+        temp = temp->left_;
+      }
+      temp = stack.Pop();
+      if (!stack.IsEmpty() && temp->right_ != NULL && stack.Top() == temp->right_) {
+        stack.Pop();
+        stack.Push(temp);
+        temp = temp->right_;
+      } else {
+        out << "[" << temp->data_ << "]->";
+        temp = NULL;
+      }
+    } while (!stack.IsEmpty());
+  }
+}
+
+template <class T>
+void BinaryTree<T>::LevelOrder(std::ostream &out, bool recursive) const {
+  if (recursive)
+    for (int i = 1; i <= Height(root_); i++) PrintLevel(out, root_, i);
+  else {
+    if (IsEmpty()) return;
+    data_structures::queues::Queue<Node *> queue(size_);
+    queue.Enqueue(root_);
+    while (!queue.IsEmpty()) {
+      Node *temp = queue.Dequeue();
+      out << "[" << temp->data_ << "]->";
+      if (temp->left_ != NULL) queue.Enqueue(temp->left_);
+      if (temp->right_ != NULL) queue.Enqueue(temp->right_);
+    }
+  }
+}
+
+template <class T>
+void BinaryTree<T>::PrintLevel(std::ostream &out, BinaryTree::Node *node, int level) const {
+  if (node == NULL) return;
+  if (level == 1)
+    out << "[" << node->data_ << "]->";
+  else if (level > 1) {
+    PrintLevel(out, node->left_, level - 1);
+    PrintLevel(out, node->right_, level - 1);
+  }
 }
 
 }  // namespace trees
