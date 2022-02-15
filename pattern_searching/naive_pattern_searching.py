785c791ee7dff360310e9b51cc4ad099cdff28a8
Khoa Nguyen <khoand27@vingroup.net>
2022-02-15 19:43:51 +0700

    searching and pattern searching solution in python, remove redundant files as well

---
+++ pattern_searching/naive_pattern_searching.py
@@ -0,0 +1,40 @@
+"""
+Naive pattern searching algorithm
+"""
+import unittest
+
+
+def naive_pattern_searching(text: str, pattern: str) -> list[int]:
+    if len(text) < 2 or len(pattern) < 2 or len(text) < len(pattern):
+        return []
+
+    ret: list[int] = []
+    for i in range(len(text) - len(pattern)):
+        matched = True
+        for j in range(len(pattern) - 1):
+            if text[i + j] != pattern[j]:
+                matched = False
+                break
+
+        if matched:
+            ret.append(i)
+
+    return ret
+
+
+class TestNaivePatternSearching(unittest.TestCase):
+
+    def test_handles_multiple_pattern_input(self):
+        self.assertListEqual(naive_pattern_searching('AABAACAADAABAAABAA', 'AABA'), [0, 9, 13])
+        self.assertListEqual(naive_pattern_searching('AAABSSSAAAOSI', 'FHJK'), [])
+        self.assertListEqual(naive_pattern_searching('AAACAAIOAAADAAIS', 'AAACA'), [0])
+
+    def test_handles_invalid_input(self):
+        self.assertListEqual(naive_pattern_searching('', ''), [])
+        self.assertListEqual(naive_pattern_searching('ABC', ''), [])
+        self.assertListEqual(naive_pattern_searching('', 'DEF'), [])
+        self.assertListEqual(naive_pattern_searching('AA', 'DEF'), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ searching/linear_search.py
@@ -0,0 +1,37 @@
+"""
+Linear search
+"""
+import unittest
+from typing import TypeVar
+
+T = TypeVar('T')
+
+
+def linear_search(sorted_array: list[T], key: T, lo: int, hi: int) -> int:
+    for i in range(len(sorted_array)):
+        if sorted_array[i] == key:
+            return i
+    return -1
+
+
+class TestLinearSearch(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestLinearSearch, self).__init__(*args, **kwargs)
+        self.integers = [2, 3, 5, 6, 8, 9, 10, 22, 26, 32, 40]
+        self.strings = ['c', 'cpp', 'go', 'java', 'python', 'sql', 'swift']
+        self.floats = [0.7, 1.2, 3.2, 4.4, 5.2, 5.9, 6.8, 9.5]
+
+    def test_handles_multiple_array_type_input(self):
+        self.assertEqual(linear_search(self.integers, 8, 0, len(self.integers) - 1), 4)
+        self.assertEqual(linear_search(self.strings, 'c', 0, len(self.strings) - 1), 0)
+        self.assertEqual(linear_search(self.floats, 9.5, 0, len(self.floats) - 1), 7)
+
+    def test_handles_non_exist_element_input(self):
+        self.assertEqual(linear_search(self.integers, 1, 0, len(self.integers) - 1), -1)
+        self.assertEqual(linear_search(self.strings, 'rust', 0, len(self.strings) - 1), -1)
+        self.assertEqual(linear_search(self.floats, 4.5, 0, len(self.floats) - 1), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
