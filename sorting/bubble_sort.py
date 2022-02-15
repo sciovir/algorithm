33fa293a88b6de8d0b4cfaa588b7bece5baca98e
Khoa Nguyen <khoand27@vingroup.net>
2022-02-15 20:11:00 +0700

    bubble sort and bucket sort in python

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,47 @@
+"""
+Bubble Sort works by repeatedly swapping the elements if they are in wrong order.
+
+Examples:
+- Giving an array [5, 2, 3, 1, 4], assuming that we sort the array by
+ascending order.
+- Starting with the first index, we compare the element at current index
+with the rest. At the end of first loop, we get the smallest element at
+current index. 0  1  2  3  4 -> array's indexes [5, 2, 3, 1, 4] we compare
+the first 2 elements then swap 5 and 2 cause 5 > 2 i  j [2, 5, 3, 1, 4] since
+these elements are in order (2 < 3), we don't swap them i     j [2, 5, 3, 1, 4]
+swap 2 and 1 i        j [1, 5, 3, 2, 4] don't swap i           j
+- Keep looping i to pre-last index and j from i + 1 to the last index.
+- After all the loops, array is now sorted.
+"""
+import unittest
+from typing import TypeVar
+
+T = TypeVar('T')
+
+
+def bubble_sort(array: list[T]):
+    for i in range(len(array)):
+        for j in range(i + 1, len(array)):
+            if array[i] > array[j]:
+                array[i], array[j] = array[j], array[i]
+
+
+class TestBubbleSort(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestBubbleSort, self).__init__(*args, **kwargs)
+        self.integers = [6, 8, 10, 26, 9, 2, 40, 22, 5, 32, 3]
+        self.strings = ['c', 'java', 'cpp', 'python', 'go', 'swift', 'sql']
+        self.floats = [5.2, 4.4, 6.8, 9.5, 0.7, 1.2, 3.2, 5.9]
+
+    def test_handles_multiple_array_type_input(self):
+        bubble_sort(self.integers)
+        self.assertListEqual(self.integers, [2, 3, 5, 6, 8, 9, 10, 22, 26, 32, 40])
+        bubble_sort(self.strings)
+        self.assertListEqual(self.strings, ['c', 'cpp', 'go', 'java', 'python', 'sql', 'swift'])
+        bubble_sort(self.floats)
+        self.assertListEqual(self.floats, [0.7, 1.2, 3.2, 4.4, 5.2, 5.9, 6.8, 9.5])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ sorting/bucket_sort.py
@@ -0,0 +1,62 @@
+"""
+Bubble Sort is mainly useful when sorting a large set of floating point
+numbers which are in range [0, 1). It works by distributing elements to
+buckets, then sorts each bucket and finally concatenates all sorted buckets.
+
+Examples:
+- Giving an array [0.52, 0.12, 0.86, 0.29, 0.46, 0.4, 0.18, 0.6], 10 buckets
+and assuming that we sort the array by ascending order.
+- Distributing all the elements to corresponding buckets by multiplying the
+element and buckets' slots. Each bucket is a linked list. [0.52, 0.12, 0.86,
+0.29, 0.46, 0.4, 0.18, 0.6] -> array
+   [[], [], [], [], [], [], [], [], [], []] -> buckets
+   [0 []
+    1 [] -> [0.12] -> [0.18]   0.12 * 10 = 1.2, 0.18 * 10 = 1.8
+    2 [] -> [0.29]   0.29 * 10 = 2.9
+    3 []
+    4 [] -> [0.46] -> [0.4]   0.46 * 10 = 4.6, 0.4 * 10 = 4.0
+    5 [] -> [0.52]   0.52 * 10 = 5.2
+    6 [] -> [0.6]
+    7 []
+    8 [] -> [0.86]   0.86 * 10 = 8.6
+    9 []
+    ]
+- We sort each bucket using insertion sort.
+   [[], [0.12, 0.18], [0.29], [], [0.4, 0.46], [0.52], [0.6], [], [0.86], []]
+- Concatenating all non-empty buckets, array is now sorted.
+"""
+import unittest
+from typing import TypeVar, Final
+
+T = TypeVar('T')
+
+
+def bucket_sort(array: list[T]):
+    SLOTS: Final = 10
+    buckets: list[list[T]] = [[] for _ in range(SLOTS)]
+
+    for i in range(len(array)):
+        bucket_index = int(SLOTS * array[i])
+        buckets[bucket_index].append(array[i])
+
+    index = 0
+    for i in range(SLOTS):
+        buckets[i].sort()
+        for j in range(len(buckets[i])):
+            array[index] = buckets[i][j]
+            index += 1
+
+
+class TestBubbleSort(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestBubbleSort, self).__init__(*args, **kwargs)
+        self.floats = [0.52, 0.44, 0.68, 0.95, 0.1, 0.12, 0.32, 0.59]
+
+    def test_handles_float_array_input(self):
+        bucket_sort(self.floats)
+        self.assertListEqual(self.floats, [0.1, 0.12, 0.32, 0.44, 0.52, 0.59, 0.68, 0.95])
+
+
+if __name__ == '__main__':
+    unittest.main()
