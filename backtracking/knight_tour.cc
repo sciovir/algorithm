28c73ce5573eaba12bc7b1266e372ae2714d736e
Khoa Nguyen <khoarx@gmail.com>
2023-01-20 22:04:46 +0700

    backtracking and remove google test

--- backtracking/knight_tour.cc
+++ backtracking/knight_tour.cc
@@ -1,21 +1,25 @@
-#include "knight_tour.h"
+#include "utils/test.h"
 
 namespace algorithm {
 namespace backtracking {
 
-bool PossibleMove(int** board, int x, int y) {
+#define CHESS_BOARD_SIZE 8
+
+bool PossibleMove(int32_t** board, int32_t x, int32_t y) {
   return x >= 0 && x < CHESS_BOARD_SIZE && y >= 0 && y < CHESS_BOARD_SIZE &&
          board[x][y] == -1;
 }
 
-bool KnightTourRecursive(int** board, const int (&x_mv)[8],
-                         const int (&y_mv)[8], int mv, int x, int y) {
+bool KnightTourRecursive(int32_t** board, const int32_t (&x_mv)[8],
+                         const int32_t (&y_mv)[8], int32_t mv, int32_t x,
+                         int32_t y) {
   if (mv == CHESS_BOARD_SIZE * CHESS_BOARD_SIZE) {
     return true;
   }
-  int x_next, y_next;
 
-  for (int i = 0; i < 8; i++) {
+  int32_t x_next, y_next;
+
+  for (auto i = 0; i < 8; i++) {
     x_next = x + x_mv[i];
     y_next = y + y_mv[i];
 
@@ -32,17 +36,17 @@ bool KnightTourRecursive(int** board, const int (&x_mv)[8],
   return false;
 }
 
-int** KnightTour(unsigned int x, unsigned int y) {
-  int** board = new int*[CHESS_BOARD_SIZE];
-  for (unsigned int r = 0; r < CHESS_BOARD_SIZE; r++) {
-    board[r] = new int[CHESS_BOARD_SIZE];
-    for (unsigned int c = 0; c < CHESS_BOARD_SIZE; c++) {
+int32_t** KnightTour(uint32_t x, uint32_t y) {
+  auto board = new int32_t*[CHESS_BOARD_SIZE];
+  for (auto r = 0; r < CHESS_BOARD_SIZE; r++) {
+    board[r] = new int32_t[CHESS_BOARD_SIZE];
+    for (auto c = 0; c < CHESS_BOARD_SIZE; c++) {
       board[r][c] = -1;
     }
   }
 
-  int x_move[] = {2, 1, -1, -2, -2, -1, 2, 1};
-  int y_move[] = {1, 2, 2, 1, -1, -2, -1, -2};
+  int32_t x_move[] = {2, 1, -1, -2, -2, -1, 2, 1};
+  int32_t y_move[] = {1, 2, 2, 1, -1, -2, -1, -2};
 
   board[0][0] = 0;
   if (KnightTourRecursive(board, x_move, y_move, 1, x, y)) {
@@ -52,5 +56,32 @@ int** KnightTour(unsigned int x, unsigned int y) {
   }
 }
 
+namespace test {
+
+void KnightTour_KnightStartFromTopLeft() {
+  int32_t answer[CHESS_BOARD_SIZE][CHESS_BOARD_SIZE] = {
+      {0, 33, 42, 63, 12, 25, 8, 21}, {41, 62, 45, 32, 43, 22, 11, 24},
+      {46, 1, 34, 13, 26, 9, 20, 7},  {61, 40, 51, 44, 31, 18, 23, 10},
+      {52, 47, 2, 35, 14, 27, 6, 19}, {39, 60, 37, 50, 55, 30, 17, 28},
+      {48, 53, 58, 3, 36, 15, 56, 5}, {59, 38, 49, 54, 57, 4, 29, 16}};
+
+  auto board = KnightTour(0, 0);
+  for (auto i = 0; i < CHESS_BOARD_SIZE; i++) {
+    for (auto j = 0; j < CHESS_BOARD_SIZE; j++) {
+      EXPECT_EQ(board[i][j], answer[i][j]);
+    }
+  }
+}
+
+void RunTests() { TEST(KnightTour_KnightStartFromTopLeft); }
+
+}  // namespace test
+
 }  // namespace backtracking
 }  // namespace algorithm
+
+int main() {
+  std::cout << "-----Running knight tour tests-----" << std::endl;
+  algorithm::backtracking::test::RunTests();
+  return 0;
+}
--- backtracking/rat_in_maze.cc
+++ backtracking/rat_in_maze.cc
@@ -1,34 +1,39 @@
-#include "rat_in_maze.h"
+#include "utils/test.h"
 
 namespace algorithm {
 namespace backtracking {
 
-bool PossibleMove(int** board, int x, int y) {
+#define MAZE_SIZE 4
+
+bool PossibleMove(int32_t** board, int32_t x, int32_t y) {
   return x >= 0 && x < MAZE_SIZE && y >= 0 && y < MAZE_SIZE && board[x][y] == 1;
 }
 
-bool RatInMazeRecursive(int** board, int** out, int x, int y, int dx, int dy) {
+bool RatInMazeRecursive(int32_t** board, int32_t** out, int32_t x, int32_t y,
+                        int32_t dx, int32_t dy) {
   if (PossibleMove(board, x, y)) {
     out[x][y] = 1;
     if (x == dx && y == dy) {
       return true;
     }
+
     if (RatInMazeRecursive(board, out, x + 1, y, dx, dy) ||
         RatInMazeRecursive(board, out, x, y + 1, dx, dy)) {
       return true;
     }
+
     out[x][y] = 0;
     return false;
   }
   return false;
 }
 
-int** RatInMaze(int** board, unsigned int x, unsigned int y, unsigned int dx,
-                unsigned int dy) {
-  int** out = new int*[MAZE_SIZE];
-  for (unsigned int r = 0; r < MAZE_SIZE; r++) {
-    out[r] = new int[8];
-    for (unsigned int c = 0; c < MAZE_SIZE; c++) {
+int32_t** RatInMaze(int32_t** board, uint32_t x, uint32_t y, uint32_t dx,
+                    uint32_t dy) {
+  auto out = new int32_t*[MAZE_SIZE];
+  for (auto r = 0; r < MAZE_SIZE; r++) {
+    out[r] = new int32_t[8];
+    for (auto c = 0; c < MAZE_SIZE; c++) {
       out[r][c] = 0;
     }
   }
@@ -40,5 +45,50 @@ int** RatInMaze(int** board, unsigned int x, unsigned int y, unsigned int dx,
   }
 }
 
+namespace test {
+
+void RatInMaze_RatStartFromTopLeftToBottomRight() {
+  auto board = new int32_t*[MAZE_SIZE];
+  for (auto r = 0; r < MAZE_SIZE; r++) {
+    board[r] = new int32_t[8];
+    for (auto c = 0; c < MAZE_SIZE; c++) {
+      board[r][c] = 0;
+    }
+  }
+
+  board[0][0] = 1;
+
+  board[1][0] = 1;
+  board[1][1] = 1;
+  board[1][3] = 1;
+
+  board[2][1] = 1;
+
+  board[3][0] = 1;
+  board[3][1] = 1;
+  board[3][2] = 1;
+  board[3][3] = 1;
+
+  int32_t answer[MAZE_SIZE][MAZE_SIZE] = {
+      {1, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 1, 1}};
+
+  int32_t** out = RatInMaze(board, 0, 0, MAZE_SIZE - 1, MAZE_SIZE - 1);
+  for (auto i = 0; i < MAZE_SIZE; i++) {
+    for (auto j = 0; j < MAZE_SIZE; j++) {
+      EXPECT_EQ(out[i][j], answer[i][j]);
+    }
+  }
+}
+
+void RunTests() { TEST(RatInMaze_RatStartFromTopLeftToBottomRight); }
+
+}  // namespace test
+
 }  // namespace backtracking
 }  // namespace algorithm
+
+int main() {
+  std::cout << "-----Running rat in maze tests-----" << std::endl;
+  algorithm::backtracking::test::RunTests();
+  return 0;
+}
